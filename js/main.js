09824921d7f89e5b2fa244e8657a239ce9ab3a9b
acbukka <thesweylis@gmail.com>
2021-11-25 16:30:42 +0700

    feat: add back-to-top arrow

--- js/main.js
+++ js/main.js
@@ -1,5 +1,47 @@
 var form = document.getElementById("my-form");
 
+// плавный скрол:
+
+$(".header__nav a, .header__cv a").on("click", function (event) {
+  event.preventDefault();
+  var id = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate({ scrollTop: top }, 1200);
+});
+
+// стрелочка to-top:
+
+function backToTop() {
+
+  let button = $('.back-to-top');
+
+  $(window).on('scroll', () => {
+    if ($(this).scrollTop() >= 350) {
+    button.fadeIn();
+  } else {
+    button.fadeOut();
+    }
+});
+
+  button.on('click', (e) => {
+    e.preventDefault();
+    $('html').animate({scrollTop: 0}, 1000)
+  })
+}
+
+backToTop();
+
+
+
+
+
+
+
+
+
+
+
+
 async function handleSubmit(event) {
   event.preventDefault();
   var status = document.getElementById("status");
